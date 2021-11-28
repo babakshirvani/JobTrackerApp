24d2170d3e8fcea82f1f769e8ec0c5d5156b1c22
Babak <babak@Babaks-MacBook-Pro.local>
2021-11-28 00:58:15 -0800

    add dependency and jobs' route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
+const Client = require('pg').Client;
+const { development } = require('./knexfile');
+const db = new Client(development['connection']);
+db.connect();
+
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
+const morgan = require('morgan');
+
+App.use(morgan('dev'));
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
+
+const jobs = require("./routes/jobs");
+App.use('/api', jobs(db));
 
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
