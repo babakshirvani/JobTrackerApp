941a665663aa8ed6c126c029fce0eac09adc844e
Babak <babak@Babaks-MacBook-Pro.local>
2021-11-28 00:05:55 -0800

    add migrations.sql

---
+++ backend/db/migrations.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE jobs (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  job_type VARCHAR(255) NOT NULL,
+  job_workplace VARCHAR(255) NOT NULL,
+  applied_at DATE NOT NULL,
+  url TEXT,
+  note TEXT
+);
+
