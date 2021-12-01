2201b54512dfa63373aad3dadd9df0afe7b77237
Babak <babak@Babaks-MacBook-Pro.local>
2021-12-01 15:43:20 -0800

    use axiso and cheerio to scrap indeed job post

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,23 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 const morgan = require('morgan');
+const axios = require('axios');
+const cheerio = require('cheerio');
+// const cors = require("cors")
+
+// App.use(cors())
+
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// app.use('/api', createProxyMiddleware({ 
+//     target: 'http://localhost:3000/', //original url
+//     changeOrigin: true, 
+//     //secure: false,
+//     onProxyRes: function (proxyRes, req, res) {
+//        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//     }
+// }));
+
+
 
 App.use(morgan('dev'));
 
@@ -23,6 +40,17 @@ App.use('/api', jobs(db));
 App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
+
+axios(`https://ca.indeed.com/viewjob?jk=5487b262f16f4a05`).then(response => {
+  const html = response.data
+  const $ = cheerio.load(html)
+  const jobTitle = $('.jobsearch-JobInfoHeader-title', html).text()
+  const jobType = $('.jobsearch-JobMetadataHeader-item').find('span').text()
+  const companyName = $('.jobsearch-CompanyReview--heading').text()
+  const jobPostDate = $('.jobsearch-JobMetadataFooter').find(1).text()
+
+  console.log(jobPostDate)
+})
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
